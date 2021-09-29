f47e1c8f4634f2e75c791d18a02bfd2d23dfbd27
Alejandro Marcano <+>
2021-09-28 23:41:49 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+//document.addEventListener("DOMContentLoaded", function() {
+  //  console.log("dom has load")
+//});
+
+// document.addEventListener("DOMContentLoaded",function(){
+//     console.log("dom has loaded")
+// });
+
+// let changeText = document.getElementById('text')
+// changeText.textContent = "This is really cool!"
+// console.log(changeText.textContent);
+
+document.addEventListener("DOMContentLoaded", function(){
+    let newText = document.getElementById("text")
+    newText.textContent = "This is really cool!"
+    console.log("dom has loaded")
+    console.log(newText.textContent)
+})
